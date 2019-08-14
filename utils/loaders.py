3669f66e7440a47df87ca041938017fd33ac36ff
shotakikuchi <goodtime683@icloud.com>
2019-08-14 23:34:54 +0900

    Implements WGAN

--- utils/loaders.py
+++ utils/loaders.py
@@ -3,6 +3,7 @@ import os
 from os import walk
 from keras.datasets import mnist
 import numpy as np
+from keras.datasets import mnist, cifar100, cifar10
 
 
 def load_mnist():
@@ -65,3 +66,20 @@ def load_safari(data_dir, folder):
         i += 1
 
     return xtotal, ytotal
+
+
+def load_cifar(label, num):
+    if num == 10:
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    else:
+        (x_train, y_train), (x_test, y_test) = cifar100.load_data(label_mode='fine')
+
+    train_mask = [y[0] == label for y in y_train]
+    test_mask = [y[0] == label for y in y_test]
+
+    x_data = np.concatenate([x_train[train_mask], x_test[test_mask]])
+    y_data = np.concatenate([y_train[train_mask], y_test[test_mask]])
+
+    x_data = (x_data.astype('float32') - 127.5) / 127.5
+
+    return (x_data, y_data)
