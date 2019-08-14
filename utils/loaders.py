475bedbb2bca89ebecd59a6c4b43a76acb059e0b
shotakikuchi <goodtime683@icloud.com>
2019-08-14 16:58:48 +0900

    Implements GAN

--- utils/loaders.py
+++ utils/loaders.py
@@ -1,6 +1,8 @@
 import pickle
 import os
+from os import walk
 from keras.datasets import mnist
+import numpy as np
 
 
 def load_mnist():
@@ -23,3 +25,43 @@ def load_model(model_class, folder):
     model.load_weights(os.path.join(folder, 'weights/weights.h5'))
 
     return model
+
+
+def load_safari(data_dir, folder):
+    # mypath = os.path.join("./data", folder)
+    mypath = os.path.join(data_dir, folder)
+    txt_name_list = []
+    for (dirpath, dirnames, filenames) in walk(mypath):
+        for f in filenames:
+            if f != '.DS_Store':
+                txt_name_list.append(f)
+                break
+
+    slice_train = int(80000 / len(txt_name_list))  ###Setting value to be 80000 for the final dataset
+    i = 0
+    seed = np.random.randint(1, 10e6)
+
+    for txt_name in txt_name_list:
+        txt_path = os.path.join(mypath, txt_name)
+        x = np.load(txt_path)
+        x = (x.astype('float32') - 127.5) / 127.5
+        # x = x.astype('float32') / 255.0
+
+        x = x.reshape(x.shape[0], 28, 28, 1)
+
+        y = [i] * len(x)
+        np.random.seed(seed)
+        np.random.shuffle(x)
+        np.random.seed(seed)
+        np.random.shuffle(y)
+        x = x[:slice_train]
+        y = y[:slice_train]
+        if i != 0:
+            xtotal = np.concatenate((x, xtotal), axis=0)
+            ytotal = np.concatenate((y, ytotal), axis=0)
+        else:
+            xtotal = x
+            ytotal = y
+        i += 1
+
+    return xtotal, ytotal
